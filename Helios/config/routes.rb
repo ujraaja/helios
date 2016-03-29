f6dd48062dd6ca737482debf92b098e9ce4def23
littlebass09 <littlebass09@tamu.edu>
2016-03-29 21:48:08 +0000

    Added beginning infrastructure from Ranjita's html

--- Helios/config/routes.rb
+++ Helios/config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
+  get 'site/index'
+  get 'site/selectStudentOrFaculty'
+  get 'site/facultyFilterSelection'
+  get 'site/studentFilterSelection'
+  get 'site/facultyOutput'
+  get 'site/studentOutput'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'site#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
