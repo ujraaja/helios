4fccbf51319ba06e18d00214a99ffbc1c4817e01
littlebass09 <littlebass09@tamu.edu>
2016-03-30 05:43:45 +0000

    MB Adjusting Structure

---
+++ app/controllers/site_controller.rb
@@ -0,0 +1,19 @@
+class SiteController < ApplicationController
+  def index
+  end
+  
+  def selectStudentOrFaculty
+  end
+  
+  def facultyFilterSelection
+  end
+  
+  def studentFilterSelection
+  end
+  
+  def facultyOutput
+  end
+  
+  def studentOutput
+  end
+end
