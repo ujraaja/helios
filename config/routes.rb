cf36e05df2cba323d964e2fb707287551c9ed829
littlebass09 <littlebass09@tamu.edu>
2016-03-30 06:40:38 +0000

    First Maybe Drop

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,9 @@
 Rails.application.routes.draw do
-  get 'site/index'
-  get 'site/selectStudentOrFaculty'
-  get 'site/facultyFilterSelection'
-  get 'site/studentFilterSelection'
-  get 'site/facultyOutput'
-  get 'site/studentOutput'
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'site#index'
+  root 'movies#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -21,6 +14,8 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
+  resources :movies
+  
   # Example resource route with options:
   #   resources :products do
   #     member do
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150809022253) do
+
+  create_table "movies", force: :cascade do |t|
+    t.string   "title"
+    t.string   "rating"
+    t.text     "description"
+    t.datetime "release_date"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
