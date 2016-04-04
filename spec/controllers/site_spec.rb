fb946cfb8bae0a1bc71749df54b83f0296f7d8fd
yongle <lyuyongle@gmail.com>
2016-04-04 16:02:24 -0500

    remove duplication

--- spec/controllers/site_spec.rb
+++ spec/controllers/site_spec.rb
@@ -8,14 +8,16 @@ RSpec.describe "SiteController", type: :controller do
 	end
 
 	describe "#index" do
+		before(:each) do
+			visit site_index_path			
+		end
+
 		it "#index" do
-			visit site_index_path
 			expect(response).to have_http_status(200)
 			expect(response).to render_template(:index)
 		end
 
 		it "redirects to selectStudentOrFaculty" do
-			visit site_index_path
 			click_on "Select"
 			expect(current_path).to eq(site_selectStudentOrFaculty_path)
 			expect(page).to have_content("Students or Faculty")
@@ -23,20 +25,22 @@ RSpec.describe "SiteController", type: :controller do
 	end
 
 	describe "#selectStudentOrFaculty" do
-		it "#selectStudentOrFaculty" do
+		before(:each) do
 			visit site_selectStudentOrFaculty_path
+		end
+
+		it "#selectStudentOrFaculty" do
 			expect(response).to have_http_status(200)
 			expect(response).to render_template(:selectStudentOrFaculty)
 		end
 
 		it "redirects to studentFilterSelection" do
-			visit site_selectStudentOrFaculty_path
 			click_on "Students"
 			expect(current_path).to eq(site_studentFilterSelection_path)
 			expect(page).to have_content("Students")
 		end
+
 		it "redirects to facultyFilterSelection" do
-			visit site_selectStudentOrFaculty_path
 			click_on "Faculty"
 			expect(current_path).to eq(site_facultyFilterSelection_path)
 			expect(page).to have_content("Faculty")
