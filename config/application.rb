eda8ab075780bc223891eb1d82377f3e0bb912d0
yongle <lyuyongle@gmail.com>
2016-04-03 22:08:32 -0500

    rspec config

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,12 @@ module Rottenpotatoes
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.generators do |g|
+        g.controller_specs false
+        g.view_specs false
+        g.helper_specs false
+        g.routing_specs false
+        g.request_specs false
+    end
   end
 end
