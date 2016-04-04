0c7d59c852c15bfcb94d3f7a5ab82dac7b903b2b
yongle <lyuyongle@gmail.com>
2016-04-04 15:14:07 -0500

    sit controller basic test and some config for rails route helper

--- spec/controllers/site_spec.rb
+++ spec/controllers/site_spec.rb
@@ -6,4 +6,40 @@ RSpec.describe "SiteController", type: :controller do
 		expect(response).to have_http_status(200)
         expect(response).to render_template(:index)
 	end
+
+	it "#index" do
+		visit site_index_path
+		expect(response).to have_http_status(200)
+		expect(response).to render_template(:index)
+	end
+
+	it "#selectStudentOrFaculty" do
+		visit site_selectStudentOrFaculty_path
+		expect(response).to have_http_status(200)
+		expect(response).to render_template(:selectStudentOrFaculty)
+	end
+
+	it "#facultyFilterSelection" do
+		visit site_facultyFilterSelection_path
+		expect(response).to have_http_status(200)
+		expect(response).to render_template(:facultyFilterSelection)
+	end
+
+	it "#studentFilterSelection" do
+		visit site_studentFilterSelection_path
+		expect(response).to have_http_status(200)
+		expect(response).to render_template(:studentFilterSelection)
+	end
+
+	it "#facultyOutput" do
+		visit site_facultyOutput_path
+		expect(response).to have_http_status(200)
+		expect(response).to render_template(:facultyOutput)
+	end
+
+	it "#studentOutput" do
+		visit site_studentOutput_path
+		expect(response).to have_http_status(200)
+		expect(response).to render_template(:studentOutput)
+	end
 end
