d32a6c1a21a552689c102513f4bcd197531db725
yongle <lyuyongle@gmail.com>
2016-04-04 15:57:10 -0500

    add redirect tests

--- spec/controllers/site_spec.rb
+++ spec/controllers/site_spec.rb
@@ -7,18 +7,42 @@ RSpec.describe "SiteController", type: :controller do
         expect(response).to render_template(:index)
 	end
 
-	it "#index" do
-		visit site_index_path
-		expect(response).to have_http_status(200)
-		expect(response).to render_template(:index)
+	describe "#index" do
+		it "#index" do
+			visit site_index_path
+			expect(response).to have_http_status(200)
+			expect(response).to render_template(:index)
+		end
+
+		it "redirects to selectStudentOrFaculty" do
+			visit site_index_path
+			click_on "Select"
+			expect(current_path).to eq(site_selectStudentOrFaculty_path)
+			expect(page).to have_content("Students or Faculty")
+		end
 	end
 
-	it "#selectStudentOrFaculty" do
-		visit site_selectStudentOrFaculty_path
-		expect(response).to have_http_status(200)
-		expect(response).to render_template(:selectStudentOrFaculty)
-	end
+	describe "#selectStudentOrFaculty" do
+		it "#selectStudentOrFaculty" do
+			visit site_selectStudentOrFaculty_path
+			expect(response).to have_http_status(200)
+			expect(response).to render_template(:selectStudentOrFaculty)
+		end
 
+		it "redirects to studentFilterSelection" do
+			visit site_selectStudentOrFaculty_path
+			click_on "Students"
+			expect(current_path).to eq(site_studentFilterSelection_path)
+			expect(page).to have_content("Students")
+		end
+		it "redirects to facultyFilterSelection" do
+			visit site_selectStudentOrFaculty_path
+			click_on "Faculty"
+			expect(current_path).to eq(site_facultyFilterSelection_path)
+			expect(page).to have_content("Faculty")
+		end
+	end
+	
 	it "#facultyFilterSelection" do
 		visit site_facultyFilterSelection_path
 		expect(response).to have_http_status(200)
