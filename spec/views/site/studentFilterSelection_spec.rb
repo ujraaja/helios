cda5ad8a816972413800480f14b9b4be4ebcc4d2
yongle <lyuyongle@gmail.com>
2016-04-05 21:49:22 -0500

    view spec test

---
+++ spec/views/site/studentFilterSelection_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+RSpec.describe "site/studentFilterSelection.html.haml", type: :view do
+	before(:each) do
+		render
+	end
+
+	it "can render" do
+		expect(rendered).to include("Students")
+		expect(rendered).to include("Who are you looking for?")
+	end	
+
+	it "include buttons" do
+		expect(rendered).to have_button("Select")
+		expect(rendered).to have_button("Apply selected filters")
+	end
+
+	xit "have select options" do
+		expect(rendered).to have_select("Gender", :options => ['Male', 'Female'])
+	end
+
+	xit "have checkbox" do
+		expect(rendered).to have_checkbox("Name")
+	end
+end
