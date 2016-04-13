662cd12bc628b9c6b49767941b83255d2abaef52
yongle <lyuyongle@gmail.com>
2016-04-12 21:02:02 -0500

    fix index view test

--- spec/views/site/index_spec.rb
+++ spec/views/site/index_spec.rb
@@ -1,9 +1,10 @@
 require "rails_helper"
 
-RSpec.describe "site/index.html.haml", type: :view do
-	xit "can render" do
-		assign(:spreadsheet, Spreadsheet.create!(:name=>"2016_students", :attachment=>"2016_students.csv"))
+RSpec.describe "site/index.html.haml", type: :view do	
+	it "can render" do
+		@spreadsheet = FactoryGirl.create(:spreadsheet)
+		@spreadsheets = Array.new(3, @spreadsheet)
 		render
 		expect(rendered).to include("Choose among existing data files")
-	end	
+	end
 end
