cf887ae59f7e04edaebeda01987fc737bb304a82
littlebass09 <littlebass09@tamu.edu>
2016-04-21 04:00:36 +0000

    MB fixed download so it only downloads whats shown

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,10 +1,21 @@
 class Student < ActiveRecord::Base
     require 'csv'
-    def self.to_csv(all_products)
+    def self.to_csv(all_products, selected_columns)
+        
+        puts "========================"
+        puts selected_columns.inspect
+        puts "========================"
+        
+        hasCount = selected_columns.find { |item| item =~ /count/i }
+        selected_columns.reject!{|item| item =~ /count/i }
+        
         CSV.generate do |csv|
-            csv << column_names
+            if hasCount
+               csv << ["Count = " + all_products.length.to_s]
+            end
+            csv << selected_columns
             all_products.each do |product|
-                csv << product.attributes.values_at(*column_names)
+                csv << product.attributes.values_at(*selected_columns)
             end
         end
     end
