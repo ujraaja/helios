cfc92c54699778e7cf2e407fa1527a560815ab24
yongle <lyuyongle@gmail.com>
2016-04-24 15:48:58 -0500

    cats spec

---
+++ spec/routing/cats_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+RSpec.describe "cats", type: :routing do
+	it "#index" do
+		expect(:get => 'cats/index').to route_to('cats#index')
+	end
+
+	it "#new" do
+		expect(:get => 'cats/new').to route_to('cats#new')
+	end
+	
+	# it "#create" do
+	# 	# get -> post
+	# 	expect(:post => 'cats/create').to route_to(
+	# 		:controller => 'cats',
+	# 		:action => 'create')
+	# end
+
+	# it "#destroy" do
+	# 	# get -> delete
+	# 	expect(:delete => 'cats/destroy').to route_to(
+	# 		:controller => 'cats',
+	# 		:action => 'destroy')
+	# end
+end
